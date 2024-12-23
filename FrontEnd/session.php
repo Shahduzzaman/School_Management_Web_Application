ec4b585ab4be85a043bf57389470ee25d6a9ff29
Shahduzzaman <mdshahduzzaman@gmail.com>
2024-12-23 16:02:02 +0600

    session updated

--- FrontEnd/session.php
+++ FrontEnd/session.php
@@ -2,13 +2,16 @@
 session_start();
 include('connect_db.php');
 
-$inactive = 10;
+$inactive = 600;
 
 if (isset($_SESSION['last_activity'])) {
     if (time() - $_SESSION['last_activity'] > $inactive) {
+        echo "<script>
+            alert('Your session has expired. You will be redirected to the login page.');
+            window.location.href = 'login.php';
+        </script>";
         session_unset();
         session_destroy();
-        header("Location: login.php");
         exit();
     }
 }
